86d0bee1f42e0ec95f3df2c8e4b1bb692ba2afc1
ysouyno <ysouyno@163.com>
2018-03-03 15:29:06 +0800

    change from out-of-source to in-source

--- cdt4.sh
+++ cdt4.sh
@@ -1,11 +1,17 @@
 #!/bin/sh
 
 # https://cmake.org/Wiki/Eclipse_CDT4_Generator
-echo 'Generating eclipse project files ...'
+#echo 'Generating eclipse project files ...'
 
-mkdir build; cd build
+# out-of-source build begin
+#mkdir build; cd build
 
 # Note: The following command must be a directory, such as "../src"",
 # do not use CMakeLists.txt instead, or the generated file and
 # CMakeLists.txt in the same directory
-cmake -G "Eclipse CDT4 - Unix Makefiles" ../src
+#cmake -G "Eclipse CDT4 - Unix Makefiles" ../src
+# out-of-source build end
+
+# in-source build begin
+cmake -G "Eclipse CDT4 - Unix Makefiles" .
+# in-source build end
---
+++ clean.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# clean cmake files
+find . -name "CMakeCache.txt" | xargs rm -rf
+find . -name "cmake_install.cmake" | xargs rm -rf
+find . -name "Makefile" | xargs rm -rf
+find . -name "CMakeFiles" | xargs rm -rf
+
+# clean eclipse project files
+find . -name ".cproject" | xargs rm -rf
+find . -name ".project" | xargs rm -rf
+
+# clean executable file
+find . -name "daobell_main" | xargs rm -rf
