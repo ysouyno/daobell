22189bbb5da2d94949487a0a3fa479101f639a61
ysouyno <ysouyno@163.com>
2018-03-04 14:22:49 +0800

    cppunit work fine with daobell library

--- test/daobell_test.cpp
+++ test/daobell_test.cpp
@@ -3,23 +3,6 @@
 #include <cppunit/TextOutputter.h>
 #include <cppunit/TestRunner.h>
 #include <cppunit/extensions/HelperMacros.h>
-#include "../src/ftp_url_parser.h"
-
-class ftp_url_parser_test : public CppUnit::TestCase
-{
-    CPPUNIT_TEST_SUITE(ftp_url_parser_test);
-    CPPUNIT_TEST(test_parse);
-    CPPUNIT_TEST_SUITE_END();
-
-public:
-    void test_parse()
-    {
-        ftp_url_parser fup("ftp://127.0.0.1:21/w@w/1024_lnk.txt");
-        CPPUNIT_ASSERT("anonymous" == fup.user_);
-    }
-};
-
-CPPUNIT_TEST_SUITE_REGISTRATION(ftp_url_parser_test);
 
 int main(int argc, char *argv[])
 {
---
+++ test/ftp_url_parser_test.cpp
@@ -0,0 +1,27 @@
+#include <cppunit/TestResult.h>
+#include <cppunit/TestResultCollector.h>
+#include <cppunit/TextOutputter.h>
+#include <cppunit/TestRunner.h>
+#include <cppunit/extensions/HelperMacros.h>
+#include "../src/ftp_url_parser.h"
+
+class ftp_url_parser_test : public CppUnit::TestCase
+{
+    CPPUNIT_TEST_SUITE(ftp_url_parser_test);
+    CPPUNIT_TEST(test_parse);
+    CPPUNIT_TEST_SUITE_END();
+
+public:
+    void test_parse();
+};
+
+CPPUNIT_TEST_SUITE_REGISTRATION(ftp_url_parser_test);
+
+void ftp_url_parser_test::test_parse()
+{
+    ftp_url_parser fup("ftp://127.0.0.1:21/w@w/1024_lnk.txt");
+    CPPUNIT_ASSERT("anonymous" == fup.user_);
+    CPPUNIT_ASSERT("anonymous" == fup.password_);
+    CPPUNIT_ASSERT("127.0.0.1" == fup.domain_);
+    CPPUNIT_ASSERT("21" == fup.port_);
+}
