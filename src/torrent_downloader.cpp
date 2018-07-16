c563a98d9f98c17ec6f373bd0a9411a89ccbd1ef
ysouyno <ysouyno@163.com>
2018-07-16 23:34:26 +0800

    no important change, just remove some useless comment

--- src/torrent_downloader.cpp
+++ src/torrent_downloader.cpp
@@ -320,8 +320,6 @@ void *connect_tracker_thread(void *arg)
     request_str += hup.domain_;
     request_str += "\r\n\r\n";
 
-    // std::cout << "request string: " << request_str << std::endl;
-
     // get socket
     int sockfd = get_tracker_socket(hup);
 
@@ -373,34 +371,15 @@ void torrent_downloader::download_it(const std::string &torrent_file, const std:
 {
   std::ifstream file(torrent_file.c_str(), std::ios::binary);
   bencode_parser bp(file);
-
   std::shared_ptr<bencode_value_base> sp_bvb = bp.get_value();
+
   auto ti = std::make_shared<torrent_info>();
   get_announce(ti.get(), dynamic_cast<bencode_dictionary *>(sp_bvb.get()));
-
-  /*
-  for (std::vector<std::string>::iterator it = ti->announce_list_.begin();
-       it != ti->announce_list_.end(); ++it) {
-    std::cout << "announce: " << it->c_str() << std::endl;
-  }
-  */
-
   get_info_hash(ti.get(), dynamic_cast<bencode_dictionary *>(sp_bvb.get()));
-
   get_peer_id(ti.get());
-
   get_files_and_size(ti.get(), dynamic_cast<bencode_dictionary *>(sp_bvb.get()));
 
-  /*
-  std::cout << "get files: " << std::endl;
-  for (std::vector<std::pair<std::string, long long> >::iterator it = ti->files_.begin();
-       it != ti->files_.end(); ++it) {
-    std::cout << "file path + name: " << it->first.c_str() << "\nlength: " << it->second << std::endl;
-  }
-  */
-
   pthread_t tid = 0;
-
   connect_tracker(&tid, ti.get());
 
   if (0 != tid) {
